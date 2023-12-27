22731c346e075c42fb2f1aa69a07106fb42f31e1
psiderman <20269983+psiderman@users.noreply.github.com>
2023-12-27 23:37:27 +0530

    link cards

---
+++ src/components/home/LinkCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  title: String,
+});
+</script>
+
+<template>
+  <div class="card card-small">
+    <div class="h-8 w-8">
+      <slot></slot>
+    </div>
+    <span>{{ title }}</span>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.card-small {
+  @apply flex flex-col justify-between p-4;
+  height: 140px !important;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
 import Leftbar from "@/components/home/Leftbar.vue";
 import WorkCard from "@/components/home/WorkCard.vue";
 import NowCard from "@/components/home/NowCard.vue";
+import LinkCard from "@/components/home/LinkCard.vue";
 </script>
 
 <template>
@@ -15,11 +16,36 @@ import NowCard from "@/components/home/NowCard.vue";
     <div class="grid min-h-fit w-full grid-cols-2 gap-10">
       <WorkCard />
       <NowCard />
-      <div class="card"></div>
       <div class="grid grid-cols-2 gap-10">
-        <div class="card card-small"></div>
-        <div class="card card-small"></div>
-        <div class="card card-small"></div>
+        <a href="https://owensans.vercel.app" target="_blank">
+          <LinkCard title="Owen Sans, font quiz">
+            <img src="@/assets/images/owensans.png" alt="owen sans logo" />
+            <img
+              src="@/assets/duotone/external-link.svg"
+              alt="link icon"
+              class="absolute right-4 top-4 h-6"
+            />
+          </LinkCard>
+        </a>
+        <a href="https://kiwi.psiderman.com" target="_blank">
+          <LinkCard title="Kiwi, personal finance">
+            <img src="@/assets/images/kiwi.png" alt="owen sans logo" />
+            <img
+              src="@/assets/duotone/external-link.svg"
+              alt="link icon"
+              class="absolute right-4 top-4 h-6"
+            />
+          </LinkCard>
+        </a>
+        <a href="https://links.psiderman.com/primer" target="_blank">
+          <LinkCard title="Personal finance 101">
+            <img src="@/assets/images/primer.png" alt="owen sans logo" />
+            <img
+              src="@/assets/duotone/external-link.svg"
+              alt="link icon"
+              class="absolute right-4 top-4 h-6"
+            /> </LinkCard
+        ></a>
       </div>
       <div class="col-span-2 h-10"></div>
     </div>
@@ -30,11 +56,8 @@ import NowCard from "@/components/home/NowCard.vue";
 .card {
   @apply relative h-80 rounded-3xl border border-white/5 bg-white/[2%];
   @apply select-none;
-  @apply grayscale transition-all duration-500 hover:grayscale-0;
-}
-
-.card-small {
-  @apply h-[140px];
+  @apply grayscale transition-all duration-200 hover:grayscale-0;
+  @apply hover:border-white/10 hover:bg-white/[4%];
 }
 
 .figma-arrow:hover {
