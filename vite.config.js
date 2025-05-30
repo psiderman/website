2130477bce72dcdad73622e1622a6e717240e8e2
psiderman <20269983+psiderman@users.noreply.github.com>
2025-05-30 20:05:03 +0530

    vercel build issues

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,15 @@ export default defineConfig({
     },
   },
 
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+  },
+
   build: {
     sourcemap: true,
   },
