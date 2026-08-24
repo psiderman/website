b4f6321de271f7a59c8950e26cd904264546db65
psiderman <20269983+psiderman@users.noreply.github.com>
2026-08-24 15:56:54 +0530

    chore: image cleanup

--- src/components/cards/FoursightCardContent.vue
+++ src/components/cards/FoursightCardContent.vue
@@ -7,7 +7,7 @@
       alt="bg"
       class="pointer-events-none absolute inset-0 z-0 h-full w-full bg-cyan-950 object-cover"
     />
-    <img src="@/assets/svg/foursight-wordmark.svg" class="pointer-events-none z-10 h-8" />
+    <img src="@/assets/home/foursight-wordmark.svg" class="pointer-events-none z-10 h-8" />
     <img src="@/assets/home/app.webp" class="pointer-events-none z-10 rounded-lg shadow-xl" />
   </div>
 </template>
